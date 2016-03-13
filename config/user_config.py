78b51afdf9ec1564d021bdd4e5195e09241f894c
Mahadi Yaacob <mahadi@gmail.com>
2016-03-13 14:27:04 +0800

    first commit

---
+++ config/user_config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+{
+'host'                : 'code.google.com',
+'user'                : 'mahadi.yaacob@gmail.com',
+'data_directory'      : './source',
+'input_name_format'   : '{problem}-{input}-{id}.in',
+'output_name_format'  : '{problem}-{input}-{id}.out',
+'source_name_format'  : ['{problem}-{input}-{id}.c'],
+}
